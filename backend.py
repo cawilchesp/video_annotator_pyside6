9d4a6480318eba9acb102b63dc5da48534b99b1f
Carlos Andrés Wilches Pérez <32644658+cawilchesp@users.noreply.github.com>
2023-11-09 19:50:02 -0500

    Update backend.py

--- backend.py
+++ backend.py
@@ -19,16 +19,6 @@ style_colors = {
 }
 
 
-def convert_cv_qt(cv_img):
-    """Convert from an opencv image to QPixmap"""
-    rgb_image = cv2.cvtColor(cv_img, cv2.COLOR_BGR2RGB)
-    h, w, ch = rgb_image.shape
-    bytes_per_line = ch * w
-    convert_to_Qt_format = QtGui.QImage(rgb_image.data, w, h, bytes_per_line, QtGui.QImage.Format.Format_RGB888)
-    p = convert_to_Qt_format.scaled(1280, 720, Qt.AspectRatioMode.KeepAspectRatio)
-    return QPixmap.fromImage(p)
-
-
 def open_video(source_file: str):
     cap = cv2.VideoCapture(source_file)
     if not cap.isOpened():
